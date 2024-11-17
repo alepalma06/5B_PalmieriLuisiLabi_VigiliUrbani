234594bfa08d64a527ce162831edf8914cd39e14
Mattia Luisi <luisimattia@itis-molinari.eu>
2024-11-17 13:10:46 +0100

    creazione input per filtro

--- componenti/navbar.js
+++ componenti/navbar.js
@@ -1,36 +1,28 @@
-export const NavBarComponent = (conf) => {
-    let template = `
-    <input type="radio" class="btn-check" name="btnradio" value="#TIPO" id="#ID" #CHECKED>
-    <label class="btn btn-outline-info btn-lg" for="#ID">#CAT</label>`
+export const NavBarComponent = () => {
     let parentElement;
-    return{
+
+    return {
         setParentElement: (pr) => {
             // FUNZIONE CHE DETERMINA DOVE POSIZIONARE LA RENDER
             parentElement = pr;
-        }
-        ,render: (form,table1) => {
-            // FUNZIONE CHE INIETTA DENTRO IL DIV
-            let html = "";
-            conf["tipologie"].forEach((tip,index) => {
-                // GENERA CODICE
-                let radioId = "radio" + index;
-                if ( index === 0) {
-                    //PER IL CHECKED [SE è IL PRIMO ALLORA SARà CHECKED]
-                    html += template.replace(/#ID/g, radioId).replace("#CAT", tip).replace("#TIPO", tip) .replace("#CHECKED", "checked");
-                } else {
-                    html += template.replace(/#ID/g,radioId) .replace("#CAT", tip).replace("#TIPO", tip).replace("#CHECKED", "");
-                }
-            });
+        },
+        render: (form, table1) => {
+            // HTML con input text e pulsante
+            const html = `
+                <div class="filter-container">
+                    <input type="text" id="addressFilter" class="form-control" placeholder="Inserisci un indirizzo">
+                    <button id="filterButton" class="btn btn-primary mt-2">Filtra</button>
+                </div>
+            `;
             parentElement.innerHTML = html;
 
-            document.querySelectorAll(".btn-check").forEach((radio) => {
-                radio.onclick = () => {
-                    form.setType(radio.value)//valore della scelta
-                    table1.setTipo(radio.value);
-                    table1.render()
-                };
-            });
+            // Eventi sul pulsante di filtro
+            document.getElementById("filterButton").onclick = () => {
+                const filterValue = document.getElementById("addressFilter").value.toLowerCase();
+                table1.filterRows(filterValue);
+                table1.render();
+            };
 
-        }
-    }
-}
+        },
+    };
+};
